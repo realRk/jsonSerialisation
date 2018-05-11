2cdf8b877faa8275438fdd1a20531d6b09c22164
Rakesh R <rakeshramesh2000@gmail.com>
2018-05-11 13:32:45 +0530

    qwert

---
+++ jsonParsing@@/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  jsonParsing@@
+//
+//  Created by Rk on 11/05/18.
+//  Copyright © 2018 Rk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    struct Course: Decodable {
+        let id: Int?
+        let name: String?
+        let link: String?
+        let imageUrl: String?
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let jsurl = "https://api.letsbuildthatapp.com/jsondecodable/courses_missing_fields"
+        let url = URL(string: jsurl)
+        
+        URLSession.shared.dataTask(with: url!){
+            ( data, resonse, error) in
+            let data = data
+            do{
+                let courses = try JSONDecoder().decode([Course].self,from :data!)
+                for array in courses{
+                    print(array.name!)
+                }
+            }
+            catch let JsonErr{
+                print(JsonErr)
+            }
+            }.resume()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
